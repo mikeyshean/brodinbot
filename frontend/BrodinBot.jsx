03d9e0caa9cdb06bc4c5b15ade6d371a3a9e87b3
Mikey Shean <mdshean2@gmail.com>
2016-04-19 12:46:37 -0400

    Remove turbolinks.

--- frontend/BrodinBot.jsx
+++ frontend/BrodinBot.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Dispatcher = require('./dispatcher/Dispatcher')
-module.exports = Dispatcher;
+
 var MyComponent = React.createClass({
   render: function () {
     return(

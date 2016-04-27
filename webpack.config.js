301ec366a80917649d813124129743c5f9701c42
Mikey Shean <mdshean2@gmail.com>
2016-04-27 15:41:33 -0400

    Compile react for prod.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var webpack = require("webpack");
+
 module.exports = {
   context: __dirname,
   entry: path.join(__dirname, 'frontend', 'ChatBot.jsx'),
@@ -21,5 +23,10 @@ module.exports = {
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx"]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };

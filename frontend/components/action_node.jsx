7e2b2ec2443dc10fecde17a1ed2568792591e3f7
Mikey Shean <mdshean2@gmail.com>
2016-04-28 00:34:43 -0400

    Add icons to Action Nodes.

--- frontend/components/action_node.jsx
+++ frontend/components/action_node.jsx
@@ -3,6 +3,10 @@ var AppConstants = require('../constants/app_constants');
 
 var ActionNode = React.createClass({
 
+  getInitialState: function () {
+    return {isHovered: false};
+  },
+
   render: function() {
     var children = this.props.childNodes;
     var childrenComponents = children.map(function(child) {
@@ -16,14 +20,36 @@ var ActionNode = React.createClass({
         </ul>
       );
     }
+
+    var editIcons;
+    if (this.state.isHovered) {
+      editIcons = (
+        <div className="icon-wrapper">
+          <span className="glyphicon glyphicon-plus node-icon"></span>
+          <span className="glyphicon glyphicon-pencil node-icon"></span>
+        </div>
+      )
+    }
+
     return (
       <ul>
         <li className="action">
-          <a>{this.props.method}</a>
+          <a onMouseEnter={this._handleHover.bind(this, true)} onMouseLeave={this._handleHover.bind(this, false)}>
+            {this.props.method}
+            {editIcons}
+          </a>
           {childrenComponents}
         </li>
       </ul>
     );
+  },
+
+  _handleHover: function (entering) {
+    if (entering) {
+      this.setState({isHovered: true})
+    } else {
+      this.setState({isHovered: false})
+    }
   }
 });
 
